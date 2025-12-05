3ba66bd80b86aabd22dc803ab462f8af7d85db19
Ran <ran_zhao@brown.edu>
2025-12-05 11:00:36 -0500

    labeler schema

--- src/models/custom_schema.py
+++ src/models/custom_schema.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
-from typing import Literal
+from typing import Literal, List
 from pydantic import BaseModel
 
 # Label Definition schema
@@ -23,3 +23,38 @@ class LabelValueDefinition(BaseModel):
         description='Default visibility: hide (hidden), warn (shown with warning), or ignore (shown normally)'
     )
     locales: list[Locale] = Field(..., description='Label text in different languages')
+
+
+# Labeller Definition schema
+# NOTE: defaults have been taken from bsky docs to demonstrate format.
+class Policies(BaseModel):
+    labelValues: List[str] = Field(
+        default=[], description="List of label names - e.g. ['porn', 'spider']"
+    )
+    labelValueDefinitions: List[LabelValueDefinition] = Field(
+        default=[], description="List of label definitions."
+    )
+
+class Labeler(BaseModel):
+    type: str = Field(
+        default="app.bsky.labeler.service",
+        description="Announces account as labeller.", 
+        alias="$type"
+    ) 
+    policies: Policies
+    subjectTypes: List[str] = Field(
+        default=["record"],
+        description= "subjectTypes can include 'record' for individual pieces of content, and 'account' for overall accounts."
+    )
+    subjectCollections: List[str] = Field(
+        default=["app.bsky.feed.post", "app.bsky.actor.profile"],
+        description="subjectCollections is a list of NSIDs of record types; if not defined, any record type is allowed"
+    )
+    reasonTypes: List[str] = Field(
+        default=["com.atproto.moderation.defs#reasonOther"], 
+        description="reasonTypes is a list of report reason codes (Lexicon references)."
+    ) 
+    createdAt: str = Field(
+        ...,
+        description="sample format: 2024-03-03T05:31:08.938Z"
+    )
